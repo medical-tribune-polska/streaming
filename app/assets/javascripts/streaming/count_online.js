40999c14de2895cb7c74104ea7e4c25bcdb72035
Michał Brzostek <m.brzostek@medical-tribune.pl>
2019-04-18 12:02:57 +0200

    Change getCurrentlyOnline function by removing async/await keywords. Why: Older versions of Safari thow errors

--- app/assets/javascripts/streaming/count_online.js
+++ app/assets/javascripts/streaming/count_online.js
@@ -1,35 +1,31 @@
 $(document).ready(function() {
   const show_online_block = $('#currently_online');
 
+  const getCurrentlyOnline = (url) => {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json, */*',
+        'Content-Type': 'application/json'
+      }
+    }).then(function (response) {
+      var contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        return response.json();
+      }
+      throw new TypeError("Oops, no JSON!");
+
+    }).then(response => {
+        show_online_block.html(response.online_without_mt + ' / ' + response.online_all);
+
+    }).catch((err) => console.warn(err))
+  };
+
   if (show_online_block.length > 0) {
-    url = show_online_block.data('url');
+    const url = show_online_block.data('url');
     getCurrentlyOnline(url);
 
     const updateEveryTime = moment.duration(30, "seconds").asMilliseconds();
     window.setInterval(() => getCurrentlyOnline(url), updateEveryTime);
-  }
-
-  async function getCurrentlyOnline(url) {
-    try {
-      const resp = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json, */*',
-          'Content-Type': 'application/json'
-        }
-      }).then(function(response) {
-        var contentType = response.headers.get("content-type");
-        if(contentType && contentType.includes("application/json")) {
-          return response;
-        }
-        throw new TypeError("Oops, no JSON!");
-      })
-      .catch((err) => console.warn(err))
-
-      const data = await resp.json()
-      show_online_block.html(data.online_without_mt + ' / ' + data.online_all)
-    } catch (err) {
-      console.warn(err)
-    }
   }
 });
